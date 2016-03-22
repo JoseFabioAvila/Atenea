6ef0aa8613a6161e21fc8245e9ad4f79779f3ea1
sejolopab <gamboavargasjosepablo@gmail.com>
2016-03-22 16:38:23 -0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Atenea/Atenea.csproj
    #	Atenea/Views/MainView.aspx

---
+++ Atenea/Models/DifinidoresDeCategoria.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Web;
+
+namespace Atenea.Models
+{
+    public class DifinidoresDeCategoria : Palabras
+    {
+        Categoria cat;
+        public DifinidoresDeCategoria(int id, String nom, Idioma idioma, Categoria categoria) : base(id, nom, idioma)
+        {
+            this.cat = categoria;
+        }
+    }
+}
---
+++ Atenea/Models/Palabras.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Atenea.Models
+{
+    public class Palabras
+    {
+        private int ID;
+        private String Nombre;
+        private String Idioma;
+
+        public Palabras(int id, String nom, Idioma idioma)
+        {
+            this.ID = id;
+            this.Nombre = nom;
+            this.Idioma = idioma;
+        }
+
+        public void setId(int ID)
+        {
+            this.ID = ID;
+        }
+
+        public int getId()
+        {
+            return this.ID;
+        }
+
+        public void setNombre(String nom)
+        {
+            this.Nombre = nom;
+        }
+
+        public String getNombre()
+        {
+            return this.Nombre;
+        }
+
+        public void setIdioma(String idioma)
+        {
+            this.Idioma = idioma;
+        }
+
+        public String getIdioma()
+        {
+            return this.Idioma;
+        }
+    }
+}
